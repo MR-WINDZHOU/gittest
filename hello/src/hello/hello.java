2e568e9db1a3d8184ef02f4e9d5c938b4ea3aa6a
MR-WINDZHOU <1092803192@qq.com>
2018-08-11 22:11:40 +0900

    Update hello.java

--- hello/src/hello/hello.java
+++ hello/src/hello/hello.java
@@ -4,7 +4,7 @@ public class hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("zhoujiafeng Hello!88888999991111111222222");
+		System.out.println("zhoujiafeng Hello!8888899999111111122222277777777");
 				
 	}
 
