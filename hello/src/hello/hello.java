5db60485218b021e07c3bd4c29266bcd24f09a82
zhoujiafeng <zhou@jia.com>
2018-08-11 21:18:09 +0900

    aaaaa

--- hello/src/hello/hello.java
+++ hello/src/hello/hello.java
@@ -4,7 +4,7 @@ public class hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("zhoujiafeng Hello!8888899999111111122222222222112222333");
+		System.out.println("zhoujiafeng Hello!");
 				
 	}
 
